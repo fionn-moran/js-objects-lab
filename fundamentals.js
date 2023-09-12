7040346d09ca7bd55b5db47e5ff7a58ea3c9a6f6
Fionn Moran <fionnmoran13@gmail.com>
2023-09-12 10:51:08 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+    { name: "Pat Smith", address: "1 Main Street" },
+    { name: "Sheila Dwyer", address: "2 High Street" }
+      ],
     type : { Make : 'Toyota',
              Model : ' Corolla ',
              CC : ' 1.8'
@@ -21,3 +26,4 @@ const aCar = {
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log(' Reg. = ' + aCar.registration.Year + aCar.registration.County + aCar.registration.Number)
   console.log("It is a " + aCar.colour.exteriorColour + " car, " + aCar.mileage + " mileage, with " + aCar.colour.interiorFabric.texture + " interior.");
+  console.log('First owner : ' + aCar.previous_owners[0].name + aCar.previous_owners[0].address )
